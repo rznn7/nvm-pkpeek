631df6c8da9d325547f1ab8fdd2895cd0ea1763f
rznn7 <jest1gabriel@gmail.com>
2025-10-07 23:47:40 +0200

    chore: init

---
+++ src/index.ts
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+import { readdir, readFile } from 'node:fs/promises';
+import { homedir } from 'node:os';
+import path from 'node:path';
+
+async function main() {
+	const nvmVersionsPath = path.join(homedir(), '.nvm', 'versions', 'node');
+	const installedNodeVersions = await readdir(nvmVersionsPath);
+
+	const nodeVersionsWithPackages = await Promise.all(
+		installedNodeVersions.map(async (versionFolder) => {
+			const nodeModulesPath = path.join(
+				nvmVersionsPath,
+				versionFolder,
+				'lib',
+				'node_modules',
+			);
+
+			const nodeModulesEntries = await readdir(nodeModulesPath);
+
+			const globalPackages = await Promise.all(
+				nodeModulesEntries.map(async (entry) => {
+					if (entry.startsWith('@')) {
+						const scopePath = path.join(nodeModulesPath, entry);
+						const packagesInScope = await readdir(scopePath);
+						const scopeInfos = await Promise.all(
+							packagesInScope.map(async (packageName) => {
+								const packagePath = path.join(
+									scopePath,
+									packageName,
+									'package.json',
+								);
+								const packageInfo = await extractPackageInfo(
+									packagePath,
+									entry,
+								);
+								return { [packageName]: packageInfo };
+							}),
+						);
+						return { [entry]: scopeInfos };
+					} else {
+						const packagePath = path.join(
+							nodeModulesPath,
+							entry,
+							'package.json',
+						);
+						const packageInfo = await extractPackageInfo(packagePath, entry);
+						return { [entry]: packageInfo };
+					}
+				}),
+			);
+
+			return {
+				version: versionFolder,
+				packages: globalPackages,
+			};
+		}),
+	);
+
+	console.dir(nodeVersionsWithPackages, { depth: null });
+}
+
+main();
+
+async function extractPackageInfo(packagePath: string, entry: string) {
+	const packageJsonContent = await readFile(packagePath, 'utf8');
+	const packageData = JSON.parse(packageJsonContent);
+	const version = packageData.version;
+
+	return { version };
+}
